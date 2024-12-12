5bf182be64796d85954e3aa75b2b2ba71750caea
lasse fritsch <lassefritsch@gmail.com>
2024-12-12 12:36:35 +0100

    calcAdd

---
+++ init.py
@@ -0,0 +1,8 @@
+def calcAdd():
+    a = int(input("Bitte gib eine Zahl ein: "))
+    b = int(input("Gibt eine weitere Zahl ein: "))
+    c = a + b
+
+    print(f"Die Summe aus {a} und {b} ist {c}")
+
+calcAdd()
