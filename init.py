338fedb8e9689695c094e863959e238b251fa418
lasse fritsch <lassefritsch@gmail.com>
2024-12-12 12:42:23 +0100

    calcMulti

--- init.py
+++ init.py
@@ -6,3 +6,12 @@ def calcAdd():
     print(f"Die Summe aus {a} und {b} ist {c}")
 
 calcAdd()
+
+def calcMulti():
+    a = int(input("Bitte gib eine Zahl ein: "))
+    b = int(input("Gibt eine weitere Zahl ein: "))
+    c = a * b
+
+    print(f"Das Produkt aus {a} und {b} ist {c}")
+
+calcMulti()
